d3c5132431466fa1be4ed8a445d5d610412b7dd1
sudhips <sudhips21@gmail.com>
2024-05-13 19:29:48 +0530

    App.tsx file changes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, SafeAreaView, NativeModules, Alert} from 'react-native';
 import styles from './styles';
 import Quiz from './Screens/Quiz';
@@ -6,20 +6,22 @@ import HeaderClass from './Components/Headers';
 import TypeScriptComponent from './Components/TypeScriptSample/TypeScriptComponent';
 
 const App = () => {
-  // console.log("NativeModules:", NativeModules.HelloWorld?.helloWorld)
-  // console.log("NativeModules add:", NativeModules.HelloWorld?.addTwoNumbers)
   interface NativeProp {
     sum: number;
   }
 
-  NativeModules.HelloWorld?.addTwoNumbers(5, 7)
-    .then(({sum}: NativeProp) => {
-      Alert.alert(sum.toString());
-    })
-    .catch((error: Error) => {
-      console.log(error);
-      Alert.alert(error?.message)
-    });
+  useEffect(() => {
+    // console.log("NativeModules:", NativeModules.HelloWorld?.helloWorld)
+    // console.log("NativeModules add:", NativeModules.HelloWorld?.addTwoNumbers)
+    NativeModules.HelloWorld?.addTwoNumbers(5, 7)
+      .then(({sum}: NativeProp) => {
+        Alert.alert(sum.toString());
+      })
+      .catch((error: Error) => {
+        console.log(error);
+        Alert.alert(error?.message);
+      });
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>

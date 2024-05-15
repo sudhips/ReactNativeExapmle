3cc8e1cdf05275896104a920259c038b3bff029a
sudhips <sudhips21@gmail.com>
2024-05-15 22:40:47 +0530

    App test crash, Track events and track events with properties for better context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, {useEffect} from 'react';
-import {View, SafeAreaView, NativeModules, Alert} from 'react-native';
+import {View, SafeAreaView, NativeModules, Alert, Button} from 'react-native';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
+
 import styles from './styles';
 import Quiz from './Screens/Quiz';
 import HeaderClass from './Components/Headers';
@@ -11,6 +14,7 @@ const App = () => {
   }
 
   useEffect(() => {
+    checkPreviousSession();
     NativeModules.HelloWorld?.addTwoNumbers(5, 7)
       .then(({sum}: NativeProp) => {
         Alert.alert(sum.toString());
@@ -21,12 +25,29 @@ const App = () => {
       });
   }, []);
 
+  const checkPreviousSession = async () => {
+    const didCrash = await Crashes.hasCrashedInLastSession();
+
+    if (didCrash) {
+      const report = await Crashes.lastSessionCrashReport();
+      Alert.alert("Sorry about that crash, we're working on a solution");
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <HeaderClass title={'Quiz Application'} />
         <Quiz />
         {/* <TypeScriptComponent /> */}
+        <Button
+          title="Testing Button"
+          onPress={
+            () => Analytics.trackEvent('testing_button', {Internet: 'WiFi', GPS: 'Off'})
+            // Crashes.generateTestCrash();
+            // throw new Error('Some Text');
+          }
+        />
       </View>
     </SafeAreaView>
   );
